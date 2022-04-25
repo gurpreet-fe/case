b30bff2e6ed56a52c62ab735f57f85ca12373d9d
Gurpreet Singh <gur.singh2022@gmail.com>
2022-04-25 22:21:41 +0100

    Cherry pick Features component

--- components/Features.js
+++ components/Features.js
@@ -7,7 +7,6 @@ import {
   Container,
 } from '@chakra-ui/react';
 import { FcDonate, FcLike, FcShare } from 'react-icons/fc';
-import H2 from './H2';
 
 const Feature = (props) => {
   return (
@@ -33,7 +32,6 @@ const Feature = (props) => {
 function Features() {
   return (
     <>
-      <H2 text='How it works' />
       <Container px={4} maxW={'7xl'} mb={8}>
         <SimpleGrid columns={{ base: 1, md: 3 }} spacing={10}>
           <Feature
