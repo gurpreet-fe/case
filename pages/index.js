fac3936399b93ae060d5d7eea507406344911b41
Gurpreet Singh <gur.singh2022@gmail.com>
2022-04-25 22:01:12 +0100

    Add Features component

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Hero from '../components/Hero';
+import Features from '../components/Features';
 export default function Home() {
   return (
     <>
       <Hero />
+      <Features />
     </>
   );
 }
