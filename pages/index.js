9d3c756b9b31f90666b32040901d2c362574a9b5
Gurpreet Singh <gur.singh2022@gmail.com>
2022-04-25 22:24:15 +0100

    Add Footer component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Hero from '../components/Hero';
 import Features from '../components/Features';
+import Footer from '../components/Footer';
 export default function Home() {
   return (
     <>
       <Hero />
       <Features />
+      <Footer />
     </>
   );
 }
